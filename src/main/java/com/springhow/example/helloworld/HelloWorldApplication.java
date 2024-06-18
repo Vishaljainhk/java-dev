3a7adcafa088f88ac42f7c2e262a62c36394c3e3
Vishal Jain H K <vishal.10741824@ltimindtree.com>
2024-06-18 12:27:42 +0530

    modified1

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "<h1>my name is vishal</h1>";
+        return "<h1>LTIMINDTREE</h1>";
     }
 
 }
